763f43b7e5ae05ddc2d235c4bef2e68967bfdb1e
wrj97 <wurongjie@deepin.org>
2026-03-23 11:20:24 +0800

    feat: update dtk to 6.7.34

--- update.go
+++ update.go
@@ -13,7 +13,7 @@ import (
 )
 
 var (
-	deepinRepoURL = "http://10.20.64.92:8080/crimson_runtime/stable_20260320"
+	deepinRepoURL = "http://10.20.64.92:8080/crimson_runtime/stable_20260323"
 	uosRepoURL    = "https://pools.uniontech.com/desktop-professional-V25"
 
 	deepinCodename = "stable"
