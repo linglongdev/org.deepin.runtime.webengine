81bc413da2ddafef06128859aa9052de16cf156f
yeshanshan <yeshanshan@uniontech.com>
2026-08-06 16:54:38 +0800

    chore: update linglong runtime to 6.7.0.46

--- build.bash
+++ build.bash
@@ -1,10 +1,11 @@
 # 安装依赖
 bash ./install_dep linglong/sources "$PREFIX"
 # 添加qmake配置
-envsubst <qt6.conf >$PREFIX/lib/qt6/bin/qt6.conf
+mkdir -p "$PREFIX/usr/lib/qt6/bin"
+envsubst <qt6.conf >"$PREFIX/usr/lib/qt6/bin/qt6.conf"
 # 设置环境变量
 mkdir -p $PREFIX/etc
 cp profile $PREFIX/etc/
 # 删除字体文件
-rm -r $PREFIX/share/fonts
-/runtime/lib/qt6/bin/qmake --version
+rm -rf "$PREFIX/share/fonts"
+/runtime/usr/lib/qt6/bin/qmake6 --version
